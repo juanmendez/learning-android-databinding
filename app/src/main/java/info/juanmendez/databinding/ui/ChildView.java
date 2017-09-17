eae79f607692004855f3f7aeea21c8b32bb45900
Juan Mendez <contact@juanmendez.info>
2017-09-17 17:58:51 -0500

    found how to do this.

--- app/src/main/java/info/juanmendez/databinding/ui/ChildView.java
+++ app/src/main/java/info/juanmendez/databinding/ui/ChildView.java
@@ -1,9 +1,11 @@
 package info.juanmendez.databinding.ui;
 
 import android.content.Context;
+import android.databinding.DataBindingUtil;
 import android.util.AttributeSet;
 import android.widget.FrameLayout;
 
+import info.juanmendez.databinding.BR;
 import info.juanmendez.databinding.databinding.ChildLayoutBinding;
 import info.juanmendez.databinding.model.DataSource;
 
@@ -11,6 +13,9 @@ import info.juanmendez.databinding.model.DataSource;
  * Created by Juan Mendez on 9/17/2017.
  * www.juanmendez.info
  * contact@juanmendez.info
+ *
+ * I fond out how to get the binding as it is already created for us.
+ * https://stackoverflow.com/questions/34799622/android-data-binding-with-a-custom-view/46269607#46269607
  */
 public class ChildView extends FrameLayout{
     private ChildLayoutBinding binding;
@@ -20,15 +25,11 @@ public class ChildView extends FrameLayout{
     }
 
     @Override
-    protected void onFinishInflate() {
-        super.onFinishInflate();
-        binding = ChildLayoutBinding.bind(this);
-    }
-
-    @Override
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
+
+        binding = DataBindingUtil.getBinding(this);
         DataSource dataSource = DataSource.get("Data View");
-        binding.setDs(dataSource);
+        binding.setVariable(BR.ds, dataSource);
     }
 }
