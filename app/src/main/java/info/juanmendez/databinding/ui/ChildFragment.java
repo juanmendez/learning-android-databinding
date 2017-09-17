c55faa215fc5a4bac409ddcadfceb03ea5bc6083
Juan Mendez <contact@juanmendez.info>
2017-09-17 15:27:50 -0500

    We made a new DataSource targetting the binding

---
+++ app/src/main/java/info/juanmendez/databinding/ui/ChildFragment.java
@@ -0,0 +1,30 @@
+package info.juanmendez.databinding.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import info.juanmendez.databinding.databinding.ChildLayoutBinding;
+import info.juanmendez.databinding.model.DataSource;
+
+/**
+ * Created by Juan Mendez on 9/17/2017.
+ * www.juanmendez.info
+ * contact@juanmendez.info
+ */
+
+public class ChildFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ChildLayoutBinding binding = ChildLayoutBinding.inflate( inflater, container, false );
+        binding.setDataSource( DataSource.get("Hello from static fragment!"));
+
+        //Returns the outermost View in the layout file associated with the Binding.
+        return binding.getRoot();
+    }
+}
